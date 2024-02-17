dbd3025f121f86ed8cff1ac726f32f7ddd4f6f7b
Rana Talukdar <147577902+RanaK1506@users.noreply.github.com>
2024-02-17 15:00:16 +0530

    Searching an element in a matrix

---
+++ StaircaseSearch.java
@@ -0,0 +1,35 @@
+//searching an element using staircase fashion
+ import java.util.*;
+public class matrics {
+    public static boolean Staircase(int matrix[][],int key) {//staircase is a search algo in matrix and others
+        int row =0,col=matrix[0].length-1;
+
+        while(row <matrix.length && col >=0) {
+            if(matrix[row][col] == key) {
+                System.out.println("element is at " + row + "," + col );
+                return true;
+            }
+            else if(key <matrix[row][col]) {
+                col--;
+            }
+            else {
+                row++;
+            }
+        }
+        System.out.println("element doesnt exist");
+        return false;
+    }
+ 
+    public static void main(String[] args) {
+       
+            int matrix[][] = {{1,2,3,4},
+                              {5,6,7,8},
+                              {9,10,11,12},
+                              {13,14,15,16}};
+        int key = 14;
+        Staircase(matrix,key);
+        
+       
+    }
+
+}
