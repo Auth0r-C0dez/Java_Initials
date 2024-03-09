320677417de9997a3e69471f91f5e8dbfd534347
Rana Talukdar <147577902+Auth0r-C0dez@users.noreply.github.com>
2024-03-09 22:58:53 +0530

    Syntax to use ternary operator in javascript.

---
+++ TernaryOperator.js
@@ -0,0 +1,7 @@
+//Ternary operator i.e..the operators that work in 3 ooperands
+
+let marks = 30;
+
+let result = marks >= 40 ? "pass" : "fail";
+console.log(result);
+//ternary operators are more compact compared to if- else statements
