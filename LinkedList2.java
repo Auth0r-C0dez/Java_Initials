2c018e90b15b20ae1cf29129dfc1fac7e8c1c8e0
Rana Talukdar <147577902+Auth0r-C0dez@users.noreply.github.com>
2024-04-18 00:36:16 +0530

    Last add and printing of linked list.

---
+++ LinkedList2.java
@@ -0,0 +1,73 @@
+public class LinkedList {
+    //time complexity of this data structure is linear
+    //creating a function for creation of node
+     public static class Node {
+        int data;
+        Node next;
+        
+        //setting the type of linked list
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+     }
+      //setting the beginning and end of LL
+     public static Node head;
+     public static Node tail;
+    //creating method to add values to nodes
+    public void addfirst(int data) {
+
+         //creating new node
+        Node newNode = new Node(data);//creating an object of class node
+
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+       
+
+        //assigning the value to the nxt node
+        newNode.next = head;//here one node is linked to another node
+
+        //assigning the value of head to new nodw
+        head = newNode;
+    }
+
+    public void addlast(int data) {
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("list is empty");
+            return;
+        }
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+
+    public static void main(String args[]) {
+        LinkedList li = new LinkedList();
+        li.print();
+        li.addfirst(1);
+        li.print();
+        li.addfirst(2);
+        li.print();
+        li.addlast(8);
+        li.print();
+        li.addlast(4);
+        li.print();
+    }
+}
