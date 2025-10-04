15b8af5cce88145d4f7d4abba6ed89200d529e8b
Rana Talukdar <147577902+Auth0r-C0dez@users.noreply.github.com>
2025-10-05 04:43:21 +0530

    Create door.java

---
+++ PLA/door.java
@@ -0,0 +1,31 @@
+package pla;
+
+public class door {
+    public static void main(String[] args) {
+        int n = 100;
+        boolean[] status = new boolean[n+1];
+        for(int i=1;i<=n;i++) {
+            for(int door = i;door<=n;door+= i) {
+                status[door] =! status[door];
+            }
+        }
+        int open =0; int close = 0;
+        for(int i=0;i<=n;i++){
+            if(status[i] == true){
+                open=open+1;}
+            else{
+                close= close+1;
+            }
+        }
+        System.out.println("Number of doors open: " + open);
+        System.out.println("Number of doors closed: " + close);
+        System.out.print("Open doors: ");
+        for (int i = 1; i <= n; i++) {
+            if (status[i]) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    
+    }
+}
