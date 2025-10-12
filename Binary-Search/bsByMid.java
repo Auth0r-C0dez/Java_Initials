ed5bfe6422182b3acaf294eee5eeee12837299da
Rana Talukdar <147577902+Auth0r-C0dez@users.noreply.github.com>
2025-10-12 12:27:20 +0530

    Create bsByMid.java

---
+++ Binary-Search/bsByMid.java
@@ -0,0 +1,25 @@
+package BinarySearch;
+
+public class bsByMid {
+    public static void main(String[] args) {
+        int[] a = {3, 4, 6, 7, 9, 12, 16, 17};
+        int target = 6;
+        int ind = binarySearch(a, target);
+        if (ind == -1)
+            System.out.println("The target is not present.");
+        else
+            System.out.println("The target is at index: " + ind);
+    }
+    public static int binarySearch(int[]arr,int target) {
+        int n = arr.length;
+        int start = 0;
+        int end = n-1;
+        while(start<=end) {
+            int mid = (start+end)/2;
+            if(arr[mid] ==target) return mid;
+            else if(target>arr[mid]) start = mid+1;
+            else end = mid-1;
+        }
+        return -1;
+    }
+}
