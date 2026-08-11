c23f04be5d04e62c4bac2743ba4ecde69ab5dfc4
Rana Talukdar <147577902+Auth0r-C0dez@users.noreply.github.com>
2026-08-11 22:48:19 +0530

    Create secondLargestElement.js

---
+++ Array/secondLargestElement.js
@@ -0,0 +1,53 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    let max = -Infinity;
+    let maxi = -Infinity;
+    for(let i = 0;i<nums.length;i++) {
+        if(nums[i] > max) {
+         maxi = max;
+         max = nums[i];
+        }else if(nums[i] > maxi && nums[i] != max) {
+            maxi = nums[i];
+            
+        }
+        
+        
+    }return maxi;
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
